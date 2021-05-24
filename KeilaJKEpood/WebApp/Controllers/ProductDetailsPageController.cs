b17c3a8298daa3df96c5664c3e393b11216d752b
jakaar <jakaar@ttu.ee>
2021-05-24 15:14:08 +0300

    login/register and item page look as they should

---
+++ KeilaJKEpood/WebApp/Controllers/ProductDetailsPageController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts.BLL.App;
+using Extensions.Base;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Models;
+#pragma warning disable 1591
+
+namespace WebApp.Controllers
+{
+
+    public class ProductDetailsPageController : Controller
+    {
+
+
+        private readonly IAppBLL _bll;
+
+        public ProductDetailsPageController(IAppBLL bll)
+        {
+            _bll = bll;
+        }
+
+        public async Task<IActionResult> Index(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _bll.Products.FirstOrDefaultAsync(id.Value, User.GetUserId()!.Value);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        public async Task<decimal> GetPrice(Guid id)
+        {
+            var prices = await _bll.Prices.GetAllAsync(User.GetUserId()!.Value);
+            foreach (var price in prices)
+            {
+                if (price.ProductId == id)
+                {
+                    return price.PriceInEur;
+                }
+            }
+
+            return 0;
+        }
+
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions()
+                {
+                    Expires = DateTimeOffset.UtcNow.AddYears(1)
+                }
+            );
+
+            return LocalRedirect(returnUrl);
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
